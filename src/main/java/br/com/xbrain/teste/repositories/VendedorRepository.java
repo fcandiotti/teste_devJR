c3dd2917328ad16a94b1e18e8a5b8c29ece0c365
fcandiotti <fernando_candiotti@hotmail.com>
2021-12-02 13:54:15 -0300

    implementado a query de filtrar as vendas por data

--- src/main/java/br/com/xbrain/teste/repositories/VendedorRepository.java
+++ src/main/java/br/com/xbrain/teste/repositories/VendedorRepository.java
@@ -1,7 +1,15 @@
 package br.com.xbrain.teste.repositories;
 
 import br.com.xbrain.teste.domain.Vendedor;
+import br.com.xbrain.teste.domain.dto.VendedorDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface VendedorRepository extends JpaRepository<Vendedor, Integer> {
+
+    @Query("SELECT DISTINCT v FROM Vendedor v LEFT JOIN FETCH v.vendas ve WHERE ve.dataVenda BETWEEN ?1 AND ?2")
+    List<VendedorDTO> filtraVendasPorPeriodo(LocalDate dataInicio, LocalDate dataFim);
 }
