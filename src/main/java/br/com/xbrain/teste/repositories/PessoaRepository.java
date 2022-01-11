8c3934fab9bf5f0837b5fbc8b5f8c58afbb93dae
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-11 14:45:54 -0300

    Removendo Enum e Perfils

--- src/main/java/br/com/xbrain/teste/repositories/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.xbrain.teste.repositories;
-
-import br.com.xbrain.teste.domain.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-}
