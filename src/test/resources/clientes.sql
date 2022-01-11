0aa1c767b817ae21055c4364b7e0f1c455f49e9f
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-11 14:50:22 -0300

    carga de dados para testes

---
+++ src/test/resources/clientes.sql
@@ -0,0 +1,2 @@
+INSERT INTO CLIENTE(ID, NOME) values (1, 'Fernando Cliente TOP');
+INSERT INTO CLIENTE(ID, NOME) values (2, 'Fernando Cliente');
---
+++ src/test/resources/vendedores.sql
@@ -0,0 +1,2 @@
+INSERT INTO VENDEDOR(ID, NOME) values (1, 'Fernando Vendedor TOP 1');
+INSERT INTO VENDEDOR(ID, NOME) values (2, 'Fernando Vendedor TOP 2');
